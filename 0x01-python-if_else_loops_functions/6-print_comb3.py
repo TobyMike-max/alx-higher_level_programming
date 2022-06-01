a4da4df0746f6a4385efd2d1d6051b0c96931871
TobyMike-max <osinubi.michael@gmail.com>
2022-06-01 11:11:54 +0100

    [Modified]: Changed string print formatter

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,5 @@ for a in range(0, 10):
     for b in range(a + 1, 10):
         if a == 8 and b == 9:
             print("{}{}".format(a, b))
-            #print(f"{a:d}{b:d}")
         else:
             print("{}{}".format(a, b), end=", ")
-            #print(f"{a:d}{b:d}", end=', ')
