d3f96847e812bfb3c1425a691f3c42dbe7c400a0
TobyMike-max <osinubi.michael@gmail.com>
2022-06-01 02:35:24 +0100

    [Modified]: Change the print string formater

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,11 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(f"{number:d} is positive")
+    print("{} is positive".format(number))
+    #print(f"{number:d} is positive")
 elif number == 0:
-    print(f"{number:d} is zero")
+    print("{} is zero".format(number))
+    #print(f"{number:d} is zero")
 else:
-    print(f"{number:d} is negative")
+    print("{} is negative".format(number))
+    #print(f"{number:d} is negative")
