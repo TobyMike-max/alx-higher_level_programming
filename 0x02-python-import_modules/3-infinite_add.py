33c352687cb4ca2255d13401bf1b6a7147c248cd
TobyMike-max <osinubi.michael@gmail.com>
2022-06-02 23:47:29 +0100

    [Modified]: Changed if/else statement to one conditional statement

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from sys import argv
+    import sys
 
     total = 0
-    count = len(argv)
-
-    if count == 1:
-        print(0)
-    else:
-        for i in range(1, count + 1):
-            total = total + int(argv[i])
-        print("{}".format(total))
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
 
