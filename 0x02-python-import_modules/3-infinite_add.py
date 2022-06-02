a61589d772a36e8cdae5abb00a6991d5d8aacdd4
TobyMike-max <osinubi.michael@gmail.com>
2022-06-02 18:42:49 +0100

    [Create]: Program that prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import argv from sys
+
+    total = 0
+    count = len(argv)
+
+    if count == 1:
+        print(0)
+    else:
+        for i in range(1, count + 1):
+            total = total + int(argv[i])
+        print("{}".format(total))
+
