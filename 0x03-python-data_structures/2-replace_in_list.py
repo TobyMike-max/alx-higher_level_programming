6e442abf1352730ae410a4d17a819b883d27315f
TobyMike-max <osinubi.michael@gmail.com>
2022-06-05 15:31:19 +0100

    [Modified]: Changed error in list name

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,5 +3,5 @@
 def replace_in_list(my_list, idx, element):
     """Replace an element of a list at a specific position."""
     if idx >= 0 and idx < len(my_list):
-        mylist[idx] = element
+        my_list[idx] = element
     return (my_list)
