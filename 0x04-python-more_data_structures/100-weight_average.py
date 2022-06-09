64e5ed37399658cd789dfecdf6885db0493954a6
TobyMike-max <osinubi.michael@gmail.com>
2022-06-09 06:05:12 +0100

    [Modified]: Changed loop iterator

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,7 +5,7 @@ def weight_average(my_list=[]):
         return (0)
 
     avg, size = 0, 0
-    for each in my_list:
+    for tup in my_list:
         avg += (tup[0] * tup[1])
         size += tup[1]
     return (avg / size)
