6552a213a533232263e0a9a7f1435efe4b997092
TobyMike-max <osinubi.michael@gmail.com>
2022-06-28 05:14:17 +0100

    [Modified]: Added docstring for new class attribute

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,9 +4,14 @@
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Represent a rectangle.
+
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+    """
     
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initialize a new Rectnagle Object.
 
