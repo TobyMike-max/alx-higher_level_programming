2ccd334eaac1fe0d3b9560feec3d4ab0161548d0
TobyMike-max <osinubi.michael@gmail.com>
2022-06-30 17:47:21 +0100

    [Modified]: Added documentation to function

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 # 4-print_square.py
+"""Function that prints a square with #."""
+
 
 def print_square(size):
+    """Prints a square.
+
+    Args:
+        size (int): The size(height/width) of the square.
+    Raises:
+        TypeError: size must be an integer.
+        ValueError: size must be >= 0.
+    """
     if type(size) != int:
         raise TypeError("size must be an integer")
     if size < 0:
