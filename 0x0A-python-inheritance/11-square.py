720cba427e69cb90c09bc8a7b57a8d747daa7455
TobyMike-max <osinubi.michael@gmail.com>
2022-07-04 20:59:32 +0100

    [Create]: class Square that inherits from Rectangle: based on task 10-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 11-square.py
+"""Define a class Square that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class defines a Square that inherits a Rectangle."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
