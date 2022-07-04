bcaf043062141ed2dfbac9f6ce198ba32465e638
TobyMike-max <osinubi.michael@gmail.com>
2022-07-04 15:22:21 +0100

    [Create]: Function that checks if object is an instance or inherited instance of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 3-is_kind_of_class.py
+"""Define function that checks if obj isinstance of class."""
+
+
+def is_same_class(obj, a_class):
+    """Checks if an object is an instance of a class or its inheritance.
+
+    Args:
+        obj (any): The object
+        a_class (type): The class to match the type of obj to.
+
+    Return:
+        If obj is exactly an instance or inheritance of a_class - True.
+        Otherwise - False.
+    """
+    if isinstance(obj, a_class):
+        return (True)
+    return (False)
