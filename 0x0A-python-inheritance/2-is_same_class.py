5011b60ec7150ff176b42f78b95944fd552afcb4
TobyMike-max <osinubi.michael@gmail.com>
2022-07-04 14:57:11 +0100

    [Create]: Function that checks if object isinstance of class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 2-is_same_class.py
+"""Define function that checks if obj isinstance of class."""
+
+
+def is_same_class(obj, a_class):
+    """Function that checks if obj isinstance of a_class.
+
+    Args:
+        obj (object): The object
+        a_class (class): The class
+
+    Return:
+        True: On success
+        False: Otherwise
+    """
+    if (isinstance(obj, a_class)):
+        return (True)
+    return (False)
