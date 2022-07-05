5e1ea7e06c3197344758a0b028b76604c6924da4
TobyMike-max <osinubi.michael@gmail.com>
2022-07-05 22:47:27 +0100

    [Create]: Function that returns the Object of a JSON representation

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 4-from_json_string.py
+"""Defines a function that returns an object(data struct) rep by JSON."""
+import json
+
+
+def from_json_string(my_str):
+    """Function that converts JSON string back to Object
+
+    Args:
+        my_str (string): The string(JSON) to convert back.
+    Returns:
+        The Object represented in JSON
+    """
+    return (json.load(my_str))
