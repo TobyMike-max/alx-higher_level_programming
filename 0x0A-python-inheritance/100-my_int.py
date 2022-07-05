2eff55a9cef2f68e27ef837ba6b2f3e296c88254
TobyMike-max <osinubi.michael@gmail.com>
2022-07-05 13:32:54 +0100

    [Create]: Function that overrides == with != and vice-versa

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 100-my_int.py
+"""Defines class MyInt that inherits from int."""
+
+
+class MyInt(int):
+    """Invert int operators == and !=."""
+
+    def __eq__(self, val):
+        """Override == operator with != behaviour."""
+        return (self.real != val)
+
+    def __ne__(self, val):
+        """Override != operator with == behaviour."""
+        return (self.real == value)
