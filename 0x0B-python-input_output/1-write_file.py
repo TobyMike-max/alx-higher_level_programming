394b95c495761e6f68203fca02ea2e2ef7a6948e
TobyMike-max <osinubi.michael@gmail.com>
2022-07-05 22:02:53 +0100

    [Modified]: Added function documentation

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,11 @@
 
 
 def read_file(filename="", text=""):
+    """Function that writes a text to a file.
+
+    Args:
+        filename (str): Name of the file to open.
+        text (str): The string of text to write to a file.
+    """
     with open(filename, r+, encoding="utf-8") as f:
         return (f.write(text))
