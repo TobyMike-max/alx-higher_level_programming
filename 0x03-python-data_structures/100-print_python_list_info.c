99aae7b69a9558e8f524d38bdbf1e852b45392ad
TobyMike-max <osinubi.michael@gmail.com>
2022-07-10 07:35:07 +0100

    [Modified]: Changed type casting to PyListObject error

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -11,7 +11,7 @@ void print_python_list_info(PyObject *p)
 	PyObject *obj;
 
 	size = Py_SIZE(p);
-	alloc = ((PyListObject *p)p)->allocated;
+	alloc = ((PyListObject *)p)->allocated;
 
 	printf("[*] Size of the Python List = %d\n", size);
 	printf("[*] Allocated = %d\n", alloc);
