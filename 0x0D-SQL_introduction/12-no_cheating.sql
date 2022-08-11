51c98516a14df313a5474b8efaa9d898b441c3f8
TobyMike-max <osinubi.michael@gmail.com>
2022-08-11 17:34:55 +0100

    [Modified]: Added missing semi-colon

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,2 @@
 -- Script that updates the score of Bob to 10 in second_table table.
-UPDATE `second_table`
-SET `score` = 10
-WHERE `name` = 'Bob'
+UPDATE `second_table` SET `score` = 10 WHERE `name` = 'Bob';
