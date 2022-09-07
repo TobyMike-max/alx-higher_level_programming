19ba40d9d2ddff252b2913bcc7a14c62ebc63f0c
TobyMike-max <osinubi.michael@gmail.com>
2022-09-07 05:58:33 +0100

    [Modified]: Removed import of argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
-if (argv.length === 2) { console.log('No argument'); } else if (argv.length === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
+const count = process.argv.length;
+if (count === 2) { console.log('No argument'); } else if (count === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
