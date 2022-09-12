04c05ec4a176fbf4e75cae40945bdeb2c18184c5
TobyMike-max <osinubi.michael@gmail.com>
2022-09-12 13:22:50 +0100

    [Modified]: Fixed query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name>  \
-#                                <state name searched>
+""" Lists all states from the database hbtn_0e_0_usa.
+        Usage: ./2-my_filter_states.py <mysql username> \
+                                        <mysql password> \
+                                        <database name>  \
+                                        <state name searched>
+"""
 
 import sys
 import MySQLdb
@@ -15,5 +16,3 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states WHERE name = '{}'".format(arg[4]))
     for state in cur.fetchall():
         print(state)
-    cur.close()
-    db.close()
