8d95e5e70299befc801a16a8e2f4448983c8a78d
TobyMike-max <osinubi.michael@gmail.com>
2022-09-12 16:36:41 +0100

    [Modified]: MySQL query fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     arg = sys.argv
     db = MySQLdb.connect(user=arg[1], passwd=arg[2], db=arg[3])
     cur = db.cursor()
-    cur.execute("SELECT c.id, c.name, s.name FROM cities as c \
-                    INNER JOIN states as s \
-                    ON c.state_id = s.id \
-                    ORDER BY c.id")
+    cur.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` FROM `cities` as `c` \
+                    INNER JOIN `states` as `s` \
+                    ON `c`.`state_id` = `s`.`id` \
+                    ORDER BY `c`.`id`")
     for state in cur.fetchall():
         print(state)
