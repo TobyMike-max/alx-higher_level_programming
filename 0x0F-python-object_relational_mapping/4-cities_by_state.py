ed2c4160664089412bb28b67623ef201f88473b6
TobyMike-max <osinubi.michael@gmail.com>
2022-09-13 08:58:27 -0700

    [Modified]: Changed file name and added sql fil

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities from database hbtn_0e_4_usa.
+Takes three arguments. Usage:
+                                ./4-cities_by_states.py
+                                <mysql username>
+                                <mysql password>
+                                <database name>
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    arg = sys.argv
+    db = MySQLdb.connect(user=arg[1], passwd=arg[2], db=arg[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities JOIN (SELECT name FROM states)")
+    for state in cur.fetchall():
+        print(state)
