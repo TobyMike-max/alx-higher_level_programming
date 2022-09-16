e6a54e8868ee91ff7d59b79163468e0f2683ca7c
TobyMike-max <osinubi.michael@gmail.com>
2022-09-16 18:45:08 +0100

    [Modified]: Pycodestyle error check

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     arg = sys.argv
     db = MySQLdb.connect(user=arg[1], passwd=arg[2], db=arg[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities 
-                JOIN states ON cities.state_id = states.id")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities
+                JOIN states ON cities.state_id=states.id")
     for state in cur.fetchall():
         print(state)
