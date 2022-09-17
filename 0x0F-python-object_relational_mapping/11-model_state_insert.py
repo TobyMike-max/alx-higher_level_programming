d4eda559c814461889dc7459cd270e84614c2031
TobyMike-max <osinubi.michael@gmail.com>
2022-09-18 00:03:01 +0100

    [Modified]: Fix pycodestyle error

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -11,15 +11,14 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-        eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                        argv[2],
-                                                                        argv[3]))
-        Base.metadata.create_all(eng)
-        Session = sessionmaker(bind=eng)
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(argv[1], argv[2], argv[3]))
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
         session = Session()
         new_state = State(name='Louisiana')
         session.add(new_state)
-        state = session.query(State).filter_by(name='Louisiana').first()
+        state = session.query(State).filter_by(State.name == 'Louisiana').first()
         print(str(state.id))
         session.commit()
         session.close()
