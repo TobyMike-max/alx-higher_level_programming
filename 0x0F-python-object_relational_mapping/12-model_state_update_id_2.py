02eb7a5ad7acf70cd24954644431679c719772e5
TobyMike-max <osinubi.michael@gmail.com>
2022-09-18 00:30:50 +0100

    [Modified]: Updates ORM query

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -11,12 +11,12 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter_by(id=2)
+    state = session.query(State).filter(State.id == 2).first()
     state.name = "New Mexico"
     session.commit()
     session.close()
