bf32e2f06a4c99f727f5374c734b375a8a593104
TobyMike-max <osinubi.michael@gmail.com>
2022-09-27 18:07:48 +0100

    [Create]: Add task 3-6

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Script that sets variable X-School-User-Id with the value 98 in response.
+curl -sH "X-School-User-Id: 98" "$1"
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Peak Finder Function in a list of unsorted integers."""
+
+def find_peak(list_of_integers):
+    """Function that finds a peak in a list of unsorted integers.
+
+    Arg:
+        list_of_integers(list) - List of all integers.
+    """
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_cpy[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
