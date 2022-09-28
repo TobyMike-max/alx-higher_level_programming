923673c6ae64c1c6571890ef91cabf58bffaebf9
TobyMike-max <osinubi.michael@gmail.com>
2022-09-28 18:16:40 +0100

    [Modified]: Module name and URL

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status."""
-import request
+import requests
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    response = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(request.text)))
-    print("\t- content: {}".format(request.text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
