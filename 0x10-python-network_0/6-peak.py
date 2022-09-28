5c2bafb40529b12389a9751665700114309c8822
TobyMike-max <osinubi.michael@gmail.com>
2022-09-28 01:32:39 +0100

    [Modified]: [Fix]: wrong list name

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -19,7 +19,7 @@ def find_peak(list_of_integers):
 
     mid = int(size / 2)
     peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_cpy[mid + 1]:
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
     elif peak < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
