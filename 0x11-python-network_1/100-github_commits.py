d95a5b293527a8861b2d264c7700e53dde9b41c9
TobyMike-max <osinubi.michael@gmail.com>
2022-09-29 03:16:24 +0100

    [Create]: Script that prints last 10 commits of a github user

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Lists the 10 most recent commits on a given GitHub repository.
+
+Usage: ./10-github_commits.py <repository name> <repository owner>
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            argv[1], argv[2])
+
+    res = requests.get(url)
+    res_json = res.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                res_json[i]["sha"],
+                res_json[i]["commit"]["author"]["name"]))
+    except IndexError:
+        pass
