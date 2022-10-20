7b73bb8f6ca60d431d8688170de8379f232c09f3
TobyMike-max <osinubi.michael@gmail.com>
2022-10-20 18:17:38 +0100

    [Modified]

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/:${id}`;
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
 request(url, (err, res, body) => {
   if (err) {
     console.log(err);
